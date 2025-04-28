68bcd7b1ad5794dca110a02a87695dbe721fdf8c
lhc-dejesusron <r.dejesus@lionheart.asia>
2025-04-28 10:59:32 +0800

    added new files

---
+++ char/index.js
@@ -0,0 +1,12 @@
+function char(str) {
+  const arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    arr.push(char);
+  }
+
+  return arr;
+}
+
+// console.log(char('hello'));
---
+++ prime/index.js
@@ -0,0 +1,16 @@
+function prime(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// console.log(prime(5));
+// big O = O(n)
